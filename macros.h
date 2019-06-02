84b032fdeece265b291d640e4eb96b828e9e0473
lliu0809 <44155608+lliu0809@users.noreply.github.com>
2019-06-02 09:26:34 -0700

    Add files via upload
    globals

---
+++ macros.h
@@ -0,0 +1,24 @@
+//
+//  macros.h
+//  
+//
+//  Created by Lihang Liu on 6/2/19.
+//
+
+#ifndef macros_h
+#define macros_h
+
+#define PACKET_SIZE 524
+#define PAYLOAD 512
+#define HEADER 12
+#define RTO 0.5
+
+#define MAX_SEQ_NUM 25600
+#define ACK_IF_NOT_SET 0
+
+#define INIT_CWND 512
+#define MAX_CWND 10240
+#define INIT_SSTHRESH 5120
+#define EC_FR 1536
+
+#endif /* macros_h */
