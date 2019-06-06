ff33055001bc3b9cbc2e2f94987ad2c06648dc4b
Luke-ZL <33585119+Luke-ZL@users.noreply.github.com>
2019-06-05 19:31:46 -0700

    Update packet.h

--- packet.h
+++ packet.h
@@ -171,7 +171,7 @@ public:
 		std::chrono::time_point<std::chrono::system_clock> end;
 		end = std::chrono::system_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - start;
-		return (elapsed_seconds.count() > 0.5);
+		return (elapsed_seconds.count() > RTO);
 	}
 };
 
